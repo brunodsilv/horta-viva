9cdb104579c8b56e093fa723ad256dfafbcf9117
brunoSoares <brunossqto13@gmail.com>
2026-05-04 22:53:32 -0300

    Configuração inicial: React, Tailwind e Firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVHbF036suuptLJ5mkmXhNESyYFHRDVOo",
+  authDomain: "horta-viva-7ec9f.firebaseapp.com",
+  projectId: "horta-viva-7ec9f",
+  storageBucket: "horta-viva-7ec9f.firebasestorage.app",
+  messagingSenderId: "984315405237",
+  appId: "1:984315405237:web:acf5894069c24931452ffc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
